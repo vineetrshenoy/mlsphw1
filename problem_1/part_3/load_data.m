b002cb167710b2b69ee8a6ef5a0dc9405e78c67f
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2020-09-26 20:39:24 -0400

    Adding problem 1 part 3

---
+++ problem_1/part_3/load_data.m
@@ -0,0 +1,20 @@
+function [train_mat] = load_training_data(direc)
+    %Loads the training data into a matrix
+
+    
+    D = dir(direc);
+    N_samples = length(D) - 2; % two hidden folders are not counted
+    train_mat = zeros(250*250, N_samples);
+    
+    count = 1;
+   
+    for i=3:length(D)
+        
+        
+        A = double(imread(fullfile(direc, D(i).name)));
+        train_mat(:, count) = A(:);
+        count = count + 1;
+
+    end
+
+end
--- problem_1/part_3/run_part3.m
+++ problem_1/part_3/run_part3.m
@@ -1,9 +1,15 @@
 %% Driver File for Problem 1: Part 3: Gender Detection
 % You will build a gender detection system using the PCA dimensions 
 % from images.
-% Author Name : (Your name here)
+% Author Name : Vineet Shenoy
 
-%% Your Driver Script Starts Here
-% You can use as many auxilliary scripts as you want
-% As long as we can run this script to get all the plots and classification
-% accuracies we require from Part 3
+clear,clc
+mens_train = load_data('../data/lfw_genders/male/train');
+womens_train = load_data('../data/lfw_genders/female/train');
+
+[h_mens, w_mens] = size(mens_train);
+[h_womens, w_womens] = size(womens_train);
+
+train_data = [mens_train, womens_train];
+
+train_data_mean = mean(train_data, 2);
